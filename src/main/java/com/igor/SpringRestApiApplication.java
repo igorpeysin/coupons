b0423220af68e44c103142a50b49c2250e5f551e
peysi <peysi@LAPTOP-3C9T4MFB>
2019-10-07 12:46:07 +0300

    version 0.1

--- src/main/java/com/igor/SpringRestApiApplication.java
+++ src/main/java/com/igor/SpringRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lecture01SpringRestApiApplication {
+public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lecture01SpringRestApiApplication.class, args);
+		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
 
 }
